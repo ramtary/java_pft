ac2694541f63aadb1fdb422055ba0a93e44c5a92
ramtary <ramtary@gmail.com>
2023-06-06 02:26:24 +0400

    Задание №2

--- sandbox/src/main/java/ru/stqa/pft/mfp/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/mfp/MyFirstProgram.java
@@ -12,10 +12,25 @@ public class MyFirstProgram {
 
 		Rectangle r = new Rectangle(5, 6);
 		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
+		// Задание №2
+		Point p1 = new Point(1, 2);
+		Point p2 = new Point(2, 1);
+		System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + distance(p1, p2));
+
+		// через метод класса (вариант 1)
+		System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + new Point().distance(p1, p2));
+
+		// через метод класса (вариант 2)
+		System.out.println("Расстояние между точками " + p1 + " и " + p2 + " = " + p1.distance(p2));
 	}
 
 	public static void helloSomebody (String somebody) {
 		System.out.println("Hello, " + somebody + "!!!11");
 	}
 
+	// Задание №2
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+	}
 }
---
+++ sandbox/src/main/java/ru/stqa/pft/mfp/Point.java
@@ -0,0 +1,37 @@
+package ru.stqa.pft.mfp;
+// Задание №2
+public class Point {
+    public int x;
+    public int y;
+
+    public Point( int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Point() {
+        this.x = 0;
+        this.y = 0;
+    }
+
+    // 1 вариант
+    public double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p2.x - p1.x, 2) + Math.pow(p2.y - p1.y, 2));
+    }
+
+    // 2 вариант
+    public double distance(Point p) {
+        return Math.sqrt(Math.pow(p.x - this.x, 2) + Math.pow(p.y - this.y, 2));
+    }
+
+
+
+    // Переопределил метод, для чуть более красивого вывода
+    @Override
+    public String toString() {
+        return  "{" +
+                "x=" + x +
+                ", y=" + y +
+                '}';
+    }
+}

1467fd209bd95894a7212fea427a1ef7973ba1f0
ramtary <ramtary@gmail.com>
2023-06-26 00:00:47 +0400

    Сравниваем списки групп целиком, при создании групп.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/models/GroupData.java
@@ -3,19 +3,20 @@ package ru.stqa.pft.addressbook.models;
 import java.util.Objects;
 
 public class GroupData {
-    private final String id;
+    private int id;
     private final String name;
+
     private final String header;
     private final String footer;
 
     public GroupData(String name, String header, String footer) {
-        this.id = null;
+        this.id = 0;
         this.name = name;
         this.header = header;
         this.footer = footer;
     }
 
-    public GroupData(String id, String name, String header, String footer) {
+    public GroupData(int id, String name, String header, String footer) {
         this.id = id;
         this.name = name;
         this.header = header;
@@ -37,18 +38,18 @@ public class GroupData {
 
         GroupData groupData = (GroupData) o;
 
-        if (!Objects.equals(id, groupData.id)) return false;
+        if (id != groupData.id) return false;
         return Objects.equals(name, groupData.name);
     }
 
     @Override
     public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
+        int result = id;
         result = 31 * result + (name != null ? name.hashCode() : 0);
         return result;
     }
 
-    public String getId() {
+    public int getId() {
         return id;
     }
 
@@ -62,5 +63,9 @@ public class GroupData {
 
     public String getFooter() {
         return footer;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }

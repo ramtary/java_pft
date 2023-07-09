36e460d9ab3baac62a6ba1dfa478bcd1e2743b76
ramtary <ramtary@gmail.com>
2023-07-10 01:14:03 +0400

    Сохраняем и читаем тестовые данные в формате xml.

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.models.GroupData;
 import ru.stqa.pft.addressbook.models.Groups;
@@ -8,6 +9,7 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.*;
 import static org.hamcrest.MatcherAssert.*;
@@ -15,7 +17,23 @@ import static org.hamcrest.MatcherAssert.*;
 
 public class GroupCreationTests extends TestBase {
     @DataProvider
-    public Iterator<Object[]> validGroups() throws IOException {
+    public Iterator<Object[]> validGroupsXml() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.xml"));
+        StringBuilder xml = new StringBuilder();
+        String line = reader.readLine();
+        while (line != null) {
+            xml.append(line);
+            line = reader.readLine();
+        }
+        XStream xStream = new XStream();
+        xStream.processAnnotations(GroupData.class);
+        xStream.allowTypes(new Class[]{GroupData.class});
+        List<GroupData> groups = (List<GroupData>) xStream.fromXML(xml.toString());
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
+    }
+
+    @DataProvider
+    public Iterator<Object[]> validGroupsCsv() throws IOException {
         List<Object[]> list = new ArrayList<>();
         BufferedReader reader = new BufferedReader(new FileReader("src/test/resources/groups.csv"));
         String line = reader.readLine();
@@ -28,7 +46,7 @@ public class GroupCreationTests extends TestBase {
         return list.iterator();
     }
 
-    @Test(dataProvider = "validGroups")
+    @Test(dataProvider = "validGroupsXml")
     public void testGroupCreation(GroupData group) {
         app.goTo().groupPage();
         Groups before = app.group().all();

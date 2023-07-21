4ba994e592e3a271a21fd9103491edde08ac91ad
ramtary <ramtary@gmail.com>
2023-07-22 00:46:02 +0400

    Добавлен тест входа в Mantis. Создан, но не завершен тест регистрации и реализована ленивая инициализация помощников.

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -15,7 +15,8 @@ public class ApplicationManager {
 
     private final String browser;
     private final Properties properties;
-    WebDriver wd;
+    private WebDriver wd;
+    private RegistrationHelper registrationHelper;
 
     public ApplicationManager(String browser) throws IOException {
         this.browser = browser;
@@ -25,20 +26,42 @@ public class ApplicationManager {
     public void init() throws IOException {
         String target = System.getProperty("target", "local");
         properties.load(new FileReader(String.format("src/test/resources/%s.properties", target)));
-
-        if (browser.equals(Browser.CHROME.browserName())) {
-            wd = new ChromeDriver();
-        } else if (browser.equals(Browser.FIREFOX.browserName())) {
-            wd = new FirefoxDriver();
-        } else if (browser.equals(Browser.IE.browserName())) {
-            wd = new InternetExplorerDriver();
-        }
-
-        wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
-        wd.get(properties.getProperty("web.baseUrl"));
     }
 
     public void stop() {
-        wd.quit();
+        if (wd != null) {
+            wd.quit();   
+        }
+    }
+
+    public HttpSession newSession() {
+        return new HttpSession(this);
+    }
+
+    public String getProperty(String key) {
+        return properties.getProperty(key);
+    }
+
+    public RegistrationHelper registration() {
+        if (registrationHelper == null) {
+            registrationHelper = new RegistrationHelper(this);
+        }
+        return registrationHelper;
+    }
+
+    public WebDriver getDriver() {
+        if (wd == null) {
+            if (browser.equals(Browser.CHROME.browserName())) {
+                wd = new ChromeDriver();
+            } else if (browser.equals(Browser.FIREFOX.browserName())) {
+                wd = new FirefoxDriver();
+            } else if (browser.equals(Browser.IE.browserName())) {
+                wd = new InternetExplorerDriver();
+            }
+
+            wd.manage().timeouts().implicitlyWait(Duration.ofSeconds(0));
+            wd.get(properties.getProperty("web.baseUrl"));
+        }
+        return wd;
     }
 }
